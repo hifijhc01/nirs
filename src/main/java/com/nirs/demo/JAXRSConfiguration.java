264cd226de06bdd0f74b7a2558addc5872273d22
Red Hat Demo Platform Student <lab-user@bastion.mtlh2.internal>
2025-12-16 01:02:46 +0000

    kitchensink(7.4.x): switch from H2 to PostgreSQL (KitchensinkDS, dialect, runtime driver)

--- src/main/java/com/nirs/demo/JAXRSConfiguration.java
+++
@@ -1,7 +0,0 @@
-package com.nirs.demo;
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/api")
-public class JAXRSConfiguration extends Application {
-}
